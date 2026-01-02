ae3146378b5a011cad6257853eba54b495fd87e9
Tom Rooker <2650021+LordDurus@users.noreply.github.com>
2026-01-02 10:32:46 -0500

    removed mut from gravity

--- game/src/physics/gravity.rs
+++ game/src/physics/gravity.rs
@@ -3,9 +3,7 @@ use crate::game::game_state::GameState;
 #[inline(always)]
 pub fn apply(game_state: &mut GameState) {
 	for (id, vel) in game_state.velocities.iter_mut() {
-		// let grav: u8 = *game_state.gravity_multipliers.get_mut(*id).unwrap_or(&mut 1);
-
-		let grav: u8 = *game_state.gravity_multipliers.get(*id).unwrap_or(&mut 1);
+		let grav: u8 = *game_state.gravity_multipliers.get(*id).unwrap_or(&1);
 
 		if grav == 0 {
 			continue;
